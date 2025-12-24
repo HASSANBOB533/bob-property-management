84fac9cd9b0a677155d45b0336ad5c6cf4338095
HASSANBOB533 <cs@bestofbedz.com>
2025-12-24 11:43:49 +0200

    Fix property images not saving to Google Sheets

--- components/PropertyForm.tsx
+++ components/PropertyForm.tsx
@@ -49,8 +49,7 @@ export default function PropertyForm() {
         data.compound = data.compoundName || '';
         
     // Fix propertyImages - use state directly instead of stringified hidden input
-    data.propertyImages = JSON.stringify(uploadedImages);
-        console.log('DEBUG: platforms =', data.platforms);
+    data.propertyImages = uploadedImages.map(img => img.url).join(', ');        console.log('DEBUG: platforms =', data.platforms);
         console.log('DEBUG: propertyImages =', data.propertyImages);
         console.log('DEBUG: uploadedImages state =', uploadedImages);
 
